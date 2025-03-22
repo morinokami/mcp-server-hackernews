ff790781ff8e4b8ca6ef78dbdc61146cca10b15c
Shinya Fujino <shf0811@gmail.com>
2025-03-22 23:59:25 +0900

    Refactor HNItem type definition to make properties optional

--- src/hackernews.ts
+++ src/hackernews.ts
@@ -1,24 +1,6 @@
 const HN_API_BASE = "https://hacker-news.firebaseio.com/v0";
 const USER_AGENT = "hackernews-app/0.0.1";
 
-type HNItem = {
-	id: number;
-	deleted?: boolean;
-	type: "job" | "story" | "comment" | "poll" | "pollopt";
-	by: string;
-	time: number;
-	text?: string;
-	dead?: boolean;
-	parent?: number;
-	poll?: number;
-	kids?: number[];
-	url?: string;
-	score?: number;
-	title?: string;
-	parts?: number[];
-	descendants?: number;
-};
-
 export type StoryType = "top" | "best" | "new";
 
 async function getStories(type: StoryType): Promise<number[]> {
@@ -44,6 +26,24 @@ export async function getBestStories() {
 export async function getNewStories() {
 	return getStories("new");
 }
+
+type HNItem = {
+	id: number;
+	deleted?: boolean;
+	type?: "job" | "story" | "comment" | "poll" | "pollopt";
+	by?: string;
+	time?: number;
+	text?: string;
+	dead?: boolean;
+	parent?: number;
+	poll?: number;
+	kids?: number[];
+	url?: string;
+	score?: number;
+	title?: string;
+	parts?: number[];
+	descendants?: number;
+};
 
 export async function getStory(id: number): Promise<HNItem> {
 	const url = `${HN_API_BASE}/item/${id}.json`;
